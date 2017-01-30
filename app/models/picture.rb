99c3730bebefafe0d43c2c03c08d81d2843947d8
Justin <justintenser@me.com>
2017-01-30 13:00:36 -0500

    Creating Picture model in database

---
+++ app/models/picture.rb
@@ -0,0 +1,11 @@
+class Picture < ApplicationRecord::Migration
+  def change
+    create_table :pictures do |t|
+      t.string :artist
+      t.string :title
+      t.string :url
+
+      t.timestamps null: false
+    end
+  end
+end
