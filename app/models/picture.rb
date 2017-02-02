e85b9b89229e11002354e87ad12939f23ea3e1a4
Justin <justintenser@me.com>
2017-02-02 15:36:41 -0500

    done

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,7 @@
 class Picture < ActiveRecord::Base
+  validates :artist, presence: true
+  validates :title, length: { minimum: 3 }
+  validates :title, length: { maximum: 20 }
+  validates :url, presence: true
+  validates :url, uniqueness: true
 end
